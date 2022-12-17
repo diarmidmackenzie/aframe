ef55c884db93a2dc6968d8046b53eb255528ca61
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-12-17 19:59:25 +0000

    Prototype fix to remove unused pool elements from THREE.js scene graph

--- src/components/scene/pool.js
+++ src/components/scene/pool.js
@@ -63,6 +63,13 @@ module.exports.Component = registerComponent('pool', {
     el.pause();
     this.container.appendChild(el);
     this.availableEls.push(el);
+
+    var usedEls = this.usedEls;
+    el.addEventListener('loaded', function () {
+      if (usedEls.indexOf(el) !== -1) { return; }
+      el.object3DParent = el.object3D.parent;
+      el.object3D.parent.remove(el.object3D);
+    });
   },
 
   /**
@@ -94,6 +101,9 @@ module.exports.Component = registerComponent('pool', {
     }
     el = this.availableEls.shift();
     this.usedEls.push(el);
+    if (el.object3DParent) {
+      el.object3DParent.add(el.object3D);
+    }
     el.object3D.visible = true;
     return el;
   },
@@ -110,6 +120,8 @@ module.exports.Component = registerComponent('pool', {
     this.usedEls.splice(index, 1);
     this.availableEls.push(el);
     el.object3D.visible = false;
+    el.object3DParent = el.object3D.parent;
+    el.object3D.parent.remove(el.object3D);
     el.pause();
     return el;
   }
