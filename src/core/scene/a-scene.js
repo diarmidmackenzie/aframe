aff6028452c2c031b5d76d8cac6e9e810fc1f4ab
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-01-18 15:05:02 +0000

    Don't use 'this' at this point in the code

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -301,7 +301,7 @@ class AScene extends AEntity {
               vrManager.setSession(xrSession).then(function () {
                 vrManager.setFoveation(rendererSystem.foveationLevel);
               });
-              this.sceneEl.systems.renderer.setWebXRFrameRate(xrSession);
+              rendererSystem.setWebXRFrameRate(xrSession);
               xrSession.addEventListener('end', self.exitVRBound);
               enterVRSuccess(resolve);
             },
